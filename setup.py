dc8ff8353feeecd75ef6a93053fca39437b3cb84
Guillaume SIMO <45049080+GuillaumeSimo@users.noreply.github.com>
2021-01-30 23:23:07 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="autoforecast", # Replace with your own username
-    version="0.0.6",
+    version="0.0.8",
     author="Guillaume Simo",
     author_email="guillaume.simo@hotmail.fr",
     description="AutoML time series forecasting",
